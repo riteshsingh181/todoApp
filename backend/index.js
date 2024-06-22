dda733158b270a46e4354e34ff3b8efa5f0a2bc3
Ritesh Singh <singhkuritesh1@gmail.com>
2024-06-22 23:01:09 +0530

    add backend routes

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,22 @@
 // with express.json() middleware
 
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 app.use(express.json());
 const port = 8080;
 
-//body {
-    // title: String,
-    // description: String;
-//}
-
 app.post("/todos", (req, res)=> {
-    const {todos} = req.query;
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs."
+        })
+        return;
+    }
+
+    //put it in mongodb;
 })
 
 app.get("/todos", (req, res)=> {
@@ -20,6 +25,13 @@ app.get("/todos", (req, res)=> {
 })
 
 app.put("/completed", (req, res)=> {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs."
+        })
+    }
 
 })
 
