8801f159a132e5ae9e907699ec394065530c97d1
Ritesh Singh <singhkuritesh1@gmail.com>
2024-06-23 09:24:22 +0530

    add db.js

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+/*
+    Todo {
+        title: string,
+        description: string,
+        completed: boolean
+
+    }
+*/
+
+const mongoose = require('mongoose');
+const { number } = require('zod');
+const mongodbURL = "mongodb+srv://ritesh:perspecta123@cluster0.vct5r3y.mongodb.net/todos";
+mongoose.connect(mongodbURL)
+
+const todoSchema = mongoose.Schema(new Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+}))
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todo
+}
