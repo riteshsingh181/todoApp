16414b479b1320c6568bcfc03698a59fd1e8afb0
Ritesh Singh <singhkuritesh1@gmail.com>
2024-06-23 10:13:00 +0530

    correct the put route

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@
 
 const express = require('express');
 const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 app.use(express.json());
-const port = 8080;
+const port = 3000;
 
-app.post("/todos", (req, res)=> {
+app.post("/todo", async (req, res)=> {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
@@ -18,13 +19,25 @@ app.post("/todos", (req, res)=> {
     }
 
     //put it in mongodb;
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo created"
+    })
 })
 
-app.get("/todos", (req, res)=> {
-
+app.get("/todos", async (req, res)=> {
+    const todos = await todo.find();
+    res.json({
+        todos
+    })
 })
 
-app.put("/completed", (req, res)=> {
+app.put("/completed", async (req, res)=> {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success){
@@ -33,6 +46,16 @@ app.put("/completed", (req, res)=> {
         })
     }
 
+    const id = updatePayload._id;
+
+    //update in mongodb
+    await todo.findByIdAndUpdate(id, {
+        completed: true
+    })
+
+    res.json({
+        msg: "Todo marked as completed"
+    })
 })
 
 app.listen(port, ()=> {
